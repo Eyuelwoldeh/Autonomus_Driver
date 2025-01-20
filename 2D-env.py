2b3f02fdca397f6e594d9bafd10879da010bd0d7
eyuelwoldehanna <eyuelwoldehanna@gmail.com>
2025-01-20 15:05:26 -0600

    Added OOP functionality

--- 2D-env.py
+++ 2D-env.py
@@ -1,40 +1,50 @@
 import pygame as pg
 
+# Initialize Pygame
 pg.init()
 
-# Set up display
 background_colour = (250, 250, 250)
 screen = pg.display.set_mode((800, 800))
-pg.display.set_caption('Driver Environment') 
+pg.display.set_caption('Environment') 
 
-# Define our cars properties
-car_x_cor = 400
-car_y_cor = 760
-car_width = 30
-car_height = 60
-car_color = (150, 150, 150)
-speed = 0.25
+class Car():
+    def __init__(self):
+        self.x_cor = 0
+        self.y_cor = 0
+        self.width = 30
+        self.height = 60
+        self.color = (150, 150, 150)
+        self.speed = 0.3
+    
+    def update(self, keys):
+        if keys[pg.K_RIGHT]:  # Move right
+            self.x_cor += self.speed
+        if keys[pg.K_LEFT]:   # Move left
+            self.x_cor -= self.speed
+        if keys[pg.K_UP]:     # Move up
+            self.y_cor -= self.speed
+        if keys[pg.K_DOWN]:   # Move down
+            self.y_cor += self.speed
 
+    def draw(self, screen):
+        pg.draw.rect(screen, self.color, (self.x_cor, self.y_cor, self.width, self.height))
+    
 
+# Game loop
 running = True
 while running: 
-    screen.fill(background_colour)
+    screen.fill(background_colour) 
+    car = Car()
+
     for event in pg.event.get():
         if event.type == pg.QUIT: 
             running = False
 
+    # Get pressed keys
     keys = pg.key.get_pressed()
-    if keys[pg.K_RIGHT]:  # Move right
-        car_x_cor += speed
-    if keys[pg.K_LEFT]:   # Move left
-        car_x_cor -= speed
-    if keys[pg.K_UP]:     # Move up
-        car_y_cor -= speed
-    if keys[pg.K_DOWN]:   # Move down
-        car_y_cor += speed
+    car.update(keys)
 
-    pg.draw.rect(screen, car_color, (car_x_cor, car_y_cor, car_width, car_height))
-
+    car.draw(screen)
     pg.display.flip()
 
 pg.quit()
